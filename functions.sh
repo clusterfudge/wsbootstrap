a0e8d5f9d7d2d434e6b77ec695e0dc098571829e
Sean Fitzgerald <sean@fitzgeralds.me>
2016-08-04 21:18:14 -0700

    Updating darwin os detection to not depend on existence of homebrew.

--- functions.sh
+++ functions.sh
@@ -11,7 +11,7 @@ if [ -n "$(which apt-get)" ]; then
     PACKAGE_INSTALLER="sudo $(which apt-get) install -y"
     PACKAGE_TYPE="deb"
     PLATFORM="linux"
-elif [ -n "$(which brew)" ]; then
+elif [ "$(uname)" == "Darwin" ]; then
     PACKAGE_INSTALLER="$(which brew) install"
     PACKAGE_TYPE="brew"
     PLATFORM="darwin"
